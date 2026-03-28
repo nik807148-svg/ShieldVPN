f3bdb8a8c0e14aff75fa689319d3090bae1ceec5
nik807148-svg <nik807148@gmail.com>
2026-03-28 12:04:55 +0500

    rebrand: update all constants URLs and appName to ShieldVPN

--- lib/core/model/constants.dart
+++ lib/core/model/constants.dart
@@ -2,15 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 abstract class Constants {
-  static const appName = "Hiddify";
-  static const githubUrl = "https://github.com/hiddify/hiddify-next";
-  static const licenseUrl = "https://github.com/hiddify/hiddify-next?tab=License-1-ov-file#readme";
-  static const githubReleasesApiUrl = "https://api.github.com/repos/hiddify/hiddify-next/releases";
-  static const githubLatestReleaseUrl = "https://github.com/hiddify/hiddify-app/releases/latest";
-  static const appCastUrl = "https://raw.githubusercontent.com/hiddify/hiddify-next/main/appcast.xml";
-  static const telegramChannelUrl = "https://t.me/hiddify";
-  static const privacyPolicyUrl = "https://hiddify.com/privacy-policy/";
-  static const termsAndConditionsUrl = "https://hiddify.com/terms/";
+  static const appName = "ShieldVPN";
+  static const githubUrl = "https://github.com/nik807148-svg/ShieldVPN";
+  static const licenseUrl = "https://github.com/nik807148-svg/ShieldVPN?tab=License-1-ov-file#readme";
+  static const githubReleasesApiUrl = "https://api.github.com/repos/nik807148-svg/ShieldVPN/releases";
+  static const githubLatestReleaseUrl = "https://github.com/nik807148-svg/ShieldVPN/releases/latest";
+  static const appCastUrl = "https://raw.githubusercontent.com/nik807148-svg/ShieldVPN/main/appcast.xml";
+  static const telegramChannelUrl = "https://t.me/shieldvpn";
+  static const privacyPolicyUrl = "https://shieldvpn.app/privacy-policy/";
+  static const termsAndConditionsUrl = "https://shieldvpn.app/terms/";
   static const cfWarpPrivacyPolicy = "https://www.cloudflare.com/application/privacypolicy/";
   static const cfWarpTermsOfService = "https://www.cloudflare.com/application/terms/";
 }
